7bcff66a10abf68d257fa40be7991641586ba114
ishika386 <ishikachoudhary03082006@gmail.com>
2026-01-31 12:11:08 +0530

    groq_key_fix

--- llm_helper.py
+++ llm_helper.py
@@ -1,11 +1,10 @@
+import os
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-import os
 
 load_dotenv()
 
-llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"),model_name="llama-3.3-70b-versatile")
+api_key = os.getenv("GROQ_API_KEY")
 
-if __name__ == "__main__":
-    response = llm.invoke("What are the two main ingredients in samosa")
-    print(response.content)
+llm = ChatGroq( groq_api_key=api_key,model_name="llama-3.3-70b-versatile")
+
